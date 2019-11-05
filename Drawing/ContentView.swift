578458b85919abaeaebb16d7e070a1fdb3110bf5
Peter Barclay <peter@peter-barclay.com>
2019-11-05 09:50:12 +0000

    Paths vs shapes in SwiftUI - Arc

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -17,14 +17,33 @@ struct Triangle: Shape {
     }
 }
 
+struct Arc: Shape {
+    var startAngle: Angle
+    var endAngle: Angle
+    var clockwise: Bool
+
+    func path(in rect: CGRect) -> Path {
+        let rotationAdjustment = Angle.degrees(90)
+        let modifiedStart = startAngle - rotationAdjustment
+        let modifiedEnd = endAngle - rotationAdjustment
+
+        var path = Path()
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY),
+                    radius: rect.width / 2,
+                    startAngle: modifiedStart,
+                    endAngle: modifiedEnd,
+                    clockwise: !clockwise)
+
+        return path
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Triangle()
-//        .fill(Color.red)
-        .stroke(Color.red,
-                style: StrokeStyle(lineWidth: 10,
-                                   lineCap: .round,
-                                   lineJoin: .round))
+        Arc(startAngle: .degrees(0),
+            endAngle: .degrees(110),
+            clockwise: true)
+        .stroke(Color.blue, lineWidth: 10)
         .frame(width: 300, height: 300)
     }
 }

f7a7cb6b8c8f260b2b9035f058d9264e531dd07b
Peter Barclay <peter@peter-barclay.com>
2019-11-06 10:59:06 +0000

    Enabling high-performance Metal rendering with drawingGroup()

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -4,21 +4,45 @@
 
 import SwiftUI
 
+struct ColorCyclingCircle: View {
+    var amount = 0.0
+    var steps = 100
+
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) { value in
+                Circle()
+                    .inset(by: CGFloat(value))
+//                    .strokeBorder(self.color(for: value, brightness: 1), lineWidth: 2)
+                    .strokeBorder(LinearGradient(gradient: Gradient(colors: [
+                        self.color(for: value, brightness: 1),
+                        self.color(for: value, brightness: 0.5)
+                    ]), startPoint: .top, endPoint: .bottom), lineWidth: 2)
+            }
+        }
+        .drawingGroup() // renders in offscreen image before putting on to screen as single rendered output (uses Metal)
+    }
+
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(self.steps) + self.amount
+
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+    }
+}
+
 struct ContentView: View {
+    @State private var colorCycle = 0.0
+
     var body: some View {
         VStack {
-            Text("Hello World")
+            ColorCyclingCircle(amount: self.colorCycle)
                 .frame(width: 300, height: 300)
-                .border(ImagePaint(image: Image("Example"),
-                                   sourceRect: CGRect(x: 0, y: 0.25,
-                                                      width: 1, height: 0.5),
-                                   scale: 0.1),
-                        width: 30)
-            Capsule()
-                .strokeBorder(ImagePaint(image: Image("Example"), scale: 0.1), lineWidth: 20)
-                .frame(width: 300, height: 200)
 
-
+            Slider(value: $colorCycle)
         }
     }
 }

292dcc2d973edb0f6f3882b516cec5cc0d511d25
Peter Barclay <peter@peter-barclay.com>
2019-11-06 10:10:50 +0000

    Transforming shapes using CGAffineTransform and even-odd fills

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -4,54 +4,58 @@
 
 import SwiftUI
 
-struct Triangle: Shape {
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
+struct Flower: Shape {
+    // How much to move this petal away from the center
+    var petalOffset: Double = -20
 
-        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
-
-        return path
-    }
-}
-
-struct Arc: InsettableShape {
-    var insetAmount: CGFloat = 0
-    var startAngle: Angle
-    var endAngle: Angle
-    var clockwise: Bool
+    // How wide to make each petal
+    var petalWidth: Double = 100
 
     func path(in rect: CGRect) -> Path {
-        let rotationAdjustment = Angle.degrees(90)
-        let modifiedStart = startAngle - rotationAdjustment
-        let modifiedEnd = endAngle - rotationAdjustment
-
+        // The path that will hold all petals
         var path = Path()
-        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY),
-                    radius: rect.width / 2 - insetAmount,
-                    startAngle: modifiedStart,
-                    endAngle: modifiedEnd,
-                    clockwise: !clockwise)
 
+        // Count from 0 up to pi * 2, moving up pi / 8 each time
+        for number in stride(from: 0, to: CGFloat.pi * 2, by: CGFloat.pi / 8) {
+            // rotate the petal by the current value of our loop
+            let rotation = CGAffineTransform(rotationAngle: number)
+
+            // move the petal to be at the center of our view
+            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
+
+            // create a path for this petal using our properties plus a fixed Y and height
+            let originalPetal = Path(ellipseIn: CGRect(x: CGFloat(petalOffset), y: 0, width: CGFloat(petalWidth), height: rect.width / 2))
+
+            // apply our rotation/position transformation to the petal
+            let rotatedPetal = originalPetal.applying(position)
+
+            // add it to our main path
+            path.addPath(rotatedPetal)
+        }
+
+        // now send the main path back
         return path
-    }
-    
-    func inset(by amount: CGFloat) -> some InsettableShape {
-        var arc = self
-        arc.insetAmount += amount
-        return arc
     }
 }
 
 struct ContentView: View {
+    @State private var petalOffset = -20.0
+    @State private var petalWidth = 100.0
+    
     var body: some View {
-        Arc(startAngle: .degrees(-90),
-            endAngle: .degrees(90),
-            clockwise: true)
-//        .stroke(Color.blue, lineWidth: 40)
-        .strokeBorder(Color.blue, lineWidth: 40)
+        VStack {
+            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
+//            .stroke(Color.red, lineWidth: 1)
+            .fill(Color.red, style: FillStyle(eoFill: true)) // even/odd rule
+
+            Text("Offset")
+            Slider(value: $petalOffset, in: -40...40)
+                .padding([.leading, .bottom, .trailing])
+
+            Text("Width")
+            Slider(value: $petalWidth, in: 0...100)
+                .padding(.horizontal)
+        }
     }
 }
 

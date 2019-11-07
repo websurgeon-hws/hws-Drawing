ee8abf2381057f36ea7f24afdd16016b41a90723
Peter Barclay <peter@peter-barclay.com>
2019-11-07 12:02:08 +0000

    Animating simple shapes with animatableData

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -4,28 +4,38 @@
 
 import SwiftUI
 
+struct Trapazoid: Shape {
+    var insetAmount: CGFloat
+    
+    var animatableData: CGFloat {
+        get { insetAmount }
+        set { self.insetAmount = newValue }
+    }
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: 0, y: rect.maxY))
+        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+        
+        return path
+    }
+}
+
 struct ContentView: View {
-    @State private var amount: CGFloat = 0.0
+    @State private var insetAmount: CGFloat = 0.0
 
     var body: some View {
-        VStack {
-            ZStack {
-                Image("PaulHudson")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 200, height: 200)
-                    .saturation(Double(amount))
-                    .blur(radius: (1 - amount) * 20)
-                
+        Trapazoid(insetAmount: insetAmount)
+            .frame(width: 200, height: 100)
+            .onTapGesture {
+                withAnimation {
+                    self.insetAmount = CGFloat.random(in: 10...90)
+                }
             }
-            .frame(width: 300, height: 300)
-            
-            Slider(value: $amount)
-                .padding()
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color.black)
-        .edgesIgnoringSafeArea(.all)
     }
 }
 

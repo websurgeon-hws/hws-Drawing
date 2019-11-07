c093e386ef66dcd076c5a6fab6cf47e5a4ea83e4
Peter Barclay <peter@peter-barclay.com>
2019-11-07 11:47:54 +0000

    Special effects in SwiftUI: blendMode

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -5,12 +5,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var amount: CGFloat = 0.0
 
     var body: some View {
-        ZStack {
-            Image("PaulHudson")
-                .colorMultiply(.red)
+        VStack {
+            ZStack {
+                Circle()
+                    .fill(Color(red: 1, green: 0, blue: 0))
+                    .frame(width: 200 * amount)
+                    .offset(x: -50, y: -80)
+                    .blendMode(.screen)
+                
+                Circle()
+                    .fill(Color(red: 0, green: 1, blue: 0))
+                    .frame(width: 200 * amount)
+                    .offset(x: 50, y: -80)
+                    .blendMode(.screen)
+                
+                Circle()
+                    .fill(Color(red: 0, green: 0, blue: 1))
+                    .frame(width: 200 * amount)
+                    .blendMode(.screen)
+            }
+            .frame(width: 300, height: 300)
+            
+            Slider(value: $amount)
+                .padding()
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.black)
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
